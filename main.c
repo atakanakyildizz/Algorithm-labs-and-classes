e4d4c8f4fe709daf3d880070199e260bf085a874
atakanakyildiz <atakanakyildiz0@gmail.com>
2025-09-24 11:07:08 +0200

    24-09-2025  Prime Number Checking

---
+++ main.c
@@ -0,0 +1,30 @@
+// Ex 1
+// Prime Number
+// Receives an integer number n from the user and evaluates if it a prime number,
+
+// Algortihm : For all numbers 1, 2 and n-1, check the number can divide them correctly
+#include <stdio.h>
+
+int main(void) {
+    int number, counter=0;
+    printf("Enter a number: ");
+    scanf("%d", &number);
+    if (number < 1) {
+        printf("The number must be greater than 0\n");
+    }
+    else {
+        for (int i = 1; i <= number-1; i++) {
+            if (number % i == 0) {
+                counter++;
+            }
+            i++;
+        }
+    }
+    if (counter == 1) {
+        printf("The number %d is a prime\n", number);
+    }
+    else {
+        printf("The number %d is not a prime number\n", number);
+    }
+    return 0;
+}
