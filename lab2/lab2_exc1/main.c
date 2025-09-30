8ffbf60775e652b49a5695faf433d118aac97ad4
atakanakyildiz <atakanakyildiz0@gmail.com>
2025-09-30 19:39:48 +0200

    The Luhn algorithm (a simple checksum formula)

--- lab2/lab2_exc1/main.c
+++ lab2/lab2_exc1/main.c
@@ -2,6 +2,7 @@
 // Luhn algorithm
 // The Luhn algorithm is a simple checksum formula used to validate a variety of identification numbers, such as credit card numbers and Canadian Social Insurance Numbers
 
+
 #include <stdio.h>
 #include <stdlib.h>
 
